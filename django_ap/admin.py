0d1f05f9f9f565020f39339abcc1e41a18b14a34
waseykhan <khan.abdulwasey99@gmail.com>
2021-01-16 18:37:27 -0500

    thirdcommit

--- django_ap/admin.py
+++ django_ap/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from django_ap.models import UseName, AccessData
 # Register your models here.
+admin.site.register(UseName)
+admin.site.register(AccessData)
--- django_ap/models.py
+++ django_ap/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class UseName(models.Model):
+    user_Name = models.CharField(max_length=60, unique=True)
+    pas = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user_Name + " " + self.pas
+
+class AccessData(models.Model):
+    user = models.ForeignKey(UseName, on_delete=models.CASCADE)
+    item = models.CharField(max_length=75)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date) + self.item + self.user
