6c052026b614f98ad5e7aeb829a9661f890c30e3
waseykhan <khan.abdulwasey99@gmail.com>
2021-01-16 22:57:38 -0500

    fifthcommit

--- django_ap/views.py
+++ django_ap/views.py
@@ -4,11 +4,28 @@ from django.views.decorators.csrf import csrf_exempt
 from django_ap.models import UseName
 import json
 
+
+
+
+
 # Create your views here.
 def index(request):
-    var = request.GET.getlist('UserName')[0]
-    pas = request.GET.getlist('pass')[0]
-    uName = UseName(user_Name="var", pas="pas")
-    uName.save()
-    result = UseName.objects.filter(user_Name=var, pas=pas)
-    return HttpResponse(result)
+    try:
+        var = request.GET.getlist('UserName')[0]
+        pas = request.GET.getlist('pass')[0]
+        #uName = UseName(user_Name="var", pas="pas")
+        result = UseName.objects.get(user_Name=var, pas=pas)
+        #print(uName.objects.all())
+        return HttpResponse(result)
+    except UseName.DoesNotExist:
+        return HttpResponse(69)
+
+
+    
+    
+
+
+
+
+
+
